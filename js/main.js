97251e6c4e3c3ee74871ee85dbc96a3aff5d013d
Abdallah Noor <125575767+abdallanoor@users.noreply.github.com>
2023-05-03 01:53:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,130 @@
+/* --------------------------------- Nav Bar --------------------------------- */
+var navTrigger = document.querySelector(".navTrigger");
+var navBar = document.querySelector(".nav-bar");
+navTrigger.onclick = function () {
+  navTrigger.classList.toggle("active");
+  navBar.classList.toggle("show");
+};
+document.onclick = function (e) {
+  if (!navTrigger.contains(e.target) && !navBar.contains(e.target)) {
+    navTrigger.classList.remove("active");
+    navBar.classList.remove("show");
+  }
+};
+/* --------------------------------- Nav Bar --------------------------------- */
+/* ------------------------------ Pricing page ------------------------------ */
+var tabs = document.querySelectorAll(".tabs li");
+var contents = document.querySelectorAll(".price-content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    // to remove uk-active class from previous tab
+    tabs.forEach((tab) => tab.classList.remove("uk-active"));
+
+    // to show content according to tab selected
+    tab.classList.add("uk-active");
+
+    // to hide previous tab content
+    contents.forEach((c) => c.classList.remove("uk-active"));
+    contents[index].classList.add("uk-active");
+  });
+});
+/* ------------------------------ Pricing page ------------------------------ */
+/* ---------------------------------- Accordion ---------------------------------- */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+// Add onclick listener to every accordion element
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function () {
+    // For toggling purposes detect if the clicked section is already "active"
+    var isActive = this.classList.contains("active");
+    // Close all accordions
+    var allAccordions = document.getElementsByClassName("accordion");
+    for (j = 0; j < allAccordions.length; j++) {
+      // Remove active class from section header
+      allAccordions[j].classList.remove("active");
+
+      // Remove the max-height class from the panel to close it
+      var panel = allAccordions[j].nextElementSibling;
+      var maxHeightValue = getStyle(panel, "maxHeight");
+
+      if (maxHeightValue !== "0px") {
+        panel.style.maxHeight = null;
+      }
+    }
+    // Toggle the clicked section using a ternary operator
+    isActive ? this.classList.remove("active") : this.classList.add("active");
+
+    // Toggle the panel element
+    var panel = this.nextElementSibling;
+    var maxHeightValue = getStyle(panel, "maxHeight");
+
+    if (maxHeightValue !== "0px") {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  };
+}
+
+function getStyle(el, styleProp) {
+  var value,
+    defaultView = (el.ownerDocument || document).defaultView;
+  // W3C standard way:
+  if (defaultView && defaultView.getComputedStyle) {
+    // sanitize property name to css notation
+    // (hypen separated words eg. font-Size)
+    styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
+    return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+  } else if (el.currentStyle) {
+    // IE
+    // sanitize property name to camelCase
+    styleProp = styleProp.replace(/\-(\w)/g, function (str, letter) {
+      return letter.toUpperCase();
+    });
+    value = el.currentStyle[styleProp];
+    // convert other units to pixels on IE
+    if (/^\d+(em|pt|%|ex)?$/i.test(value)) {
+      return (function (value) {
+        var oldLeft = el.style.left,
+          oldRsLeft = el.runtimeStyle.left;
+        el.runtimeStyle.left = el.currentStyle.left;
+        el.style.left = value || 0;
+        value = el.style.pixelLeft + "px";
+        el.style.left = oldLeft;
+        el.runtimeStyle.left = oldRsLeft;
+        return value;
+      })(value);
+    }
+    return value;
+  }
+}
+/* ---------------------------------- Accordion ---------------------------------- */
+/* ---------------------------------- go up --------------------------------- */
+var goUp = document.querySelector(".go-up");
+window.onscroll = function () {
+  if (this.scrollY >= 75) {
+    goUp.classList.add("show");
+  } else {
+    goUp.classList.remove("show");
+  }
+};
+goUp.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+/* ---------------------------------- go up --------------------------------- */
+/* -------------------------------- WhatsApp -------------------------------- */
+var wtsappIcon = document.querySelector(".wtsapp-icon");
+var whatsappWidget = document.querySelector(".whatsapp-Widget");
+var close = document.querySelector(".close");
+wtsappIcon.onclick = function () {
+  whatsappWidget.classList.toggle("show");
+};
+close.onclick = function () {
+  whatsappWidget.classList.remove("show");
+};
+/* -------------------------------- WhatsApp -------------------------------- */
---
+++ js/translations.js
@@ -0,0 +1,43 @@
+const translations = {
+  en: {
+    home: "HOME",
+    about: "ABOUT",
+    courses: "COURSES",
+    pricing: "PRICING",
+    contact: "CONTACT",
+    english: "English",
+    arabic: "Arabic",
+    bookfree: "BOOK FREE EVALUATION",
+    //
+    welcome: "Welcome To",
+    iwanacademy: "IWAN ACADEMY",
+    learn: "Learn Qur’an, Arabic And Islamic Studies.",
+    ourCourses: "Our Courses",
+    contactUs: "Contact Us",
+    //
+    aboutIwan: "About Iwan Academy",
+    aboutIwan1: `Iwan Academy is ready to help you whether you want to learn holy quran, reading, memorizing or learnstandard Arabic language, and Islamic studies. through online one-to-one sessions. We can work with your needs to ensure the best learning environment for you or your kids.`,
+    aboutIwan2: "About Iwan Academy",
+    aboutIwan3: "About Iwan Academy",
+  },
+  ar: {
+    home: "الصفحة الرئيسية",
+    about: "من نحن",
+    courses: "الكورسات",
+    pricing: "الاسعار",
+    contact: "تواصل معنا",
+    selectLanguage: "إختر لغة",
+    english: "الانجليزية",
+    arabic: "العربية",
+    bookfree: "حجز ميعاد مجاني",
+    //
+    welcome: "مرحبا بك في",
+    iwanacademy: "أكاديميه إيوان",
+    learn: "تعلم القرآن والعربية والدراسات الإسلامية.",
+    ourCourses: "الكورسات",
+    contactUs: "تواصل معنا",
+    //
+  },
+};
+
+export default translations;
