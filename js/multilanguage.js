8a8b15d78de328c0bbcdf98073774f40a6976611
Abdallah Noor <125575767+abdallanoor@users.noreply.github.com>
2023-05-04 09:01:39 +0300

    Add files via upload

--- js/multilanguage.js
+++ js/multilanguage.js
@@ -7,8 +7,19 @@ languageSelector.addEventListener("change", (event) => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  const language = localStorage.getItem("lang") || "en"; // اذا لم تكن اللغة متوفرة استخدم الانجليزية
+  const language = localStorage.getItem("lang") || "en";
   setLanguage(language);
+  //
+  if (localStorage.getItem("lang") == "ar") {
+    var select = document.getElementById("languageselect");
+    var option;
+    for (var i = 0; i < select.options.length; i++) {
+      option = select.options[i];
+      if (option.value == "ar") {
+        option.selected = true;
+      }
+    }
+  }
 });
 
 const setLanguage = (language) => {
@@ -17,7 +28,34 @@ const setLanguage = (language) => {
     const translationKey = element.getAttribute("data-i18n");
     element.textContent = translations[language][translationKey];
   });
+  //Style
+  //dir
   document.dir = language === "ar" ? "rtl" : "ltr";
+  //dir - navbar
   var navBar = document.querySelector(".nav-bar");
   navBar.dir = language === "ar" ? "rtl" : "ltr";
+  //letterSpacing - a
+  var spaceOme = document.querySelectorAll("a");
+  for (var i = 0; i < spaceOme.length; i++) {
+    spaceOme[i].style.letterSpacing = language === "ar" ? "0" : "1px";
+  }
+  //Footer - h4
+  var spaceTwo = document.querySelectorAll("h4");
+  for (var i = 0; i < spaceTwo.length; i++) {
+    spaceTwo[i].style.letterSpacing = language === "ar" ? "0" : "1.6px";
+    spaceTwo[i].style.fontSize = language === "ar" ? "19px" : "16px";
+  }
+  //contactIcons - Margin
+  var contactIcons = document.querySelectorAll(".contactIcons");
+  for (var i = 0; i < contactIcons.length; i++) {
+    contactIcons[i].style.marginLeft = language === "ar" ? "10px" : "0px";
+    contactIcons[i].style.marginRight = language === "ar" ? "0px" : "10px";
+  }
+  //change font size in website > arabic
+  var all = document.querySelector("*");
+  all.style.fontSize = language === "ar" ? "17px" : "16px";
+
+  //Simble In How Works
+  var simble = document.querySelector(".simple");
+  simble.style.letterSpacing = language === "ar" ? "0" : "1px";
 };
--- js/translations.js
+++ js/translations.js
@@ -1,42 +1,224 @@
 const translations = {
   en: {
-    home: "HOME",
-    about: "ABOUT",
-    courses: "COURSES",
-    pricing: "PRICING",
-    contact: "CONTACT",
+    topHome: "Iwan Academy | Learn Qur’an, Arabic And Islamic Studies",
+    //navbar
+    home: "Home",
+    about: "About Us",
+    courses: "Courses",
+    pricing: "Pricing",
+    contact: "Contact",
     english: "English",
     arabic: "Arabic",
     bookfree: "BOOK FREE EVALUATION",
-    //
+    //Landing
     welcome: "Welcome To",
-    iwanacademy: "IWAN ACADEMY",
+    iwanacademy: "Iwan Academy",
     learn: "Learn Qur’an, Arabic And Islamic Studies.",
     ourCourses: "Our Courses",
     contactUs: "Contact Us",
-    //
-    aboutIwan: "About Iwan Academy",
-    aboutIwan1: `Iwan Academy is ready to help you whether you want to learn holy quran, reading, memorizing or learnstandard Arabic language, and Islamic studies. through online one-to-one sessions. We can work with your needs to ensure the best learning environment for you or your kids.`,
-    aboutIwan2: "About Iwan Academy",
-    aboutIwan3: "About Iwan Academy",
+    //About Section
+    hAbout: "About Iwan Academy",
+    hAbout1:
+      "Iwan Academy is an educational online academy in order to serve online Qur’an, Tajweed, Arabic Language, and Islamic Studies to Arabs & non-Arabic speakers; with an interesting technique in teaching through online one-to-one sessions, we can work with your needs to ensure the best learning environment for you or your kids.",
+    aboutMin:
+      "Iwan Academy is an educational online academy in order to serve online Qur’an, Tajweed, Arabic Language, and Islamic Studies to Arabs & non-Arabic speakers.",
+    hAbout2: "Our Tutors",
+    hAbout3:
+      "we know that learning is easier when you have an excellent teacher. Our instructors are native Egyptian Tutors qualified and graduated from Al-Azhar Al-Sharif University and they are passionate about the courses they teach and bring this enthusiasm into their classes and students.",
+    hAbout4: "Read More",
+    //Courses Section
+    hCourses: "Our Courses",
+    hCoursesDes:
+      "we offer variety of courses whether you’re looking for a beginner, or advanced course. All of our courses are customizable for your needs to make sure you have reached your goals.",
+    //Islamic Studies
+    islamic: "Islamic Studies",
+    Aqidah: "Aqidah (Islamic Creed) :",
+    Seerah: "Seerah and Islamic History :",
+    Tafseer: "Tafseer (exegesis) :",
+    Fiqh: "Fiqh :",
+    Hadith: "Hadith :",
+    Manners: "Islamic Manners For Children :",
+    //Arabic Studies
+    arabicStudies: "Arabic Studies",
+    grammar: "Arabic grammar :",
+    Conversation: "Arabic Conversation :",
+    arabicChildren: "Arabic for Children :",
+    arabicQuran: "Arabic for Quran :",
+    arabicModern: "Modern Standard Arabic :",
+    alSarf: "AL-Sarf :",
+    egyptianDialect: "The Egyptian dialect :",
+    //Quran
+    quran: "Qur’an",
+    quranRecitation: "Quran Recitation with Tajweed :",
+    quranMemorizing: "Memorizing Quran with understanding :",
+    Tajweed: "Tajweed Rules :",
+    //Why Section
+    hwhy: "WHY IWAN?",
+    whyOne: "One-on-One",
+    whyOneDes: "Modern fun learning in One-on-One classes.",
+    whyPro: "Professional Tutors",
+    whyProDes:
+      "Al-Azhar Al-Sharif certified male and female professional tutors are available",
+    whyFun: "Learn With Fun",
+    whyFunDes: "Reward based learning using the best methods and technologies",
+    whyFlex: "Flexible Schedule",
+    whyFlexDes: "Learn from anywhere and according to your schedule",
+    whyFree: "Free Demo Class",
+    whyFreeDes: "Get your first free demo class and no payment is required",
+
+    //How Works Section
+    hHowWork: "HOW IT WORKS",
+    howWorkPlan: "Planning",
+    howWorkPlanDes: "Pick a course and create your goal.",
+    howWorkSched: "Scheduling",
+    howWorkSchedDes: "Share your availability and make your schedule.",
+    howWorkStudy: "Studying",
+    howWorkStudyDes: "Pay and start your classes.",
+    howWorkSimple: "IT REALLY IS THAT SIMPLE!",
+    howWorkStart: "GET STARTED",
+    //FAQS
+    faqs: "FAQS",
+    faqOne: "What does Iwan mean?",
+    faqOneDes:
+      "The Arabic word “Iwan” means Hall. Iwan Is found in many of the marvelous mosques around the world. it is serving a number of functions including, prayer, teaching, lodging of teachers and students, keeping books and as a reading library as well as a number of charitable activities in stopping stations on the road of pilgrims intended to serve the ill and the needy. More importantly it became the area in which traditional circles of knowledge formed and classes would gather to pass down the sacred sciences, generation to generation. Even today many great mosques around the world hold classes under their “Iwans.”",
+    faqTwo: "How Can I Start?",
+    faqTwoDes:
+      "There will be an interview process to ensure the student can benefit from the services. Please contact us if you are unsure whether you meet the prerequisite and for more details.",
+    faqThree: "What software or technical equipment do I need?",
+    faqThreeDes:
+      "All classes are conducted are conducted via Zoom, so you will need to download Zoom on your laptop, tablet or smartphone. We also suggest you invest in some good quality headphones, as well as a good internet connection.",
+    faqFour: "Are the teachers ljazah Certified?",
+    faqFourDes:
+      "Yes, all of our teachers are ljazah certified and Huffaz. They also graduated from reputed Islamic Universities.",
+    faqFive: "Can I reschedule classes?",
+    faqFiveDes:
+      "We are happy to reschedule a class if you give us 24 hours notice. All students receive our rescheduling policy upon registering.",
+    faqSix: "Does Shaykh teach women?",
+    faqSixDes: "Yes. Please contact for more details.",
+    faqSeven: "Do you offer financial assistance?",
+    faqSevenDes:
+      "As a new academy, we can only offer very limited financial assistant. Please contact us if you will struggle with payment and we will do what we can to help. We aim to make our services affordable, and we will teach several advanced students for free every year, upon interview. Please contact us for more information. If you would like to sponsor a student, please contact us.",
+    //Footer
+    getTouch: "Get in touch",
+    copyRight: "Copyright © 2023. Iwan Academy. All Rights Reserved.",
+    //Whatsapp
+    repliesWithin: "Typically replies within a day",
+    asSalamu: "As-salamu alaykum!",
+    WhatsMessage:
+      "Click below to chat with us on WhatsApp. We're available to answer any questions that you may have about the Iwan Academy.",
+    WhatsChat: "Start Chat",
   },
+
   ar: {
+    topHome: "أكاديميه إيوان | تعلم القرآن والعربية والدراسات الإسلامية",
+    //navbar
     home: "الصفحة الرئيسية",
-    about: "من نحن",
-    courses: "الكورسات",
+    about: "عن إيوان",
+    courses: "ماذا نُقدِّم",
     pricing: "الاسعار",
     contact: "تواصل معنا",
     selectLanguage: "إختر لغة",
     english: "الانجليزية",
     arabic: "العربية",
-    bookfree: "حجز ميعاد مجاني",
-    //
-    welcome: "مرحبا بك في",
+    bookfree: "حجز تقييم مجاني",
+    //Landing
+    welcome: "مرحبًا بكم في",
     iwanacademy: "أكاديميه إيوان",
     learn: "تعلم القرآن والعربية والدراسات الإسلامية.",
-    ourCourses: "الكورسات",
+    ourCourses: "ماذا نُقدِّم",
     contactUs: "تواصل معنا",
-    //
+    //About Section
+    hAbout: "حول أكاديمية إيوان",
+    hAbout1:
+      "أكاديمية إيوان هي أكاديمية تعليمية على الإنترنت تهدف إلى تقديم القرآن الكريم والتجويد واللغة العربية والدراسات الإسلامية للعرب وغير الناطقين باللغة العربية ؛ من خلال أسلوب مثير للاهتمام في التدريس من خلال جلسات فردية عبر الإنترنت ، يمكننا العمل مع احتياجاتك لضمان أفضل بيئة تعليمية لك أو لأطفالك.",
+    aboutMin:
+      "أكاديمية إيوان هي أكاديمية تعليمية عبر الإنترنت لتقديم القرآن الكريم والتجويد واللغة العربية والدراسات الإسلامية للعرب وغير الناطقين باللغة العربية.",
+    hAbout2: "فريق إيوان",
+    hAbout3:
+      " مجموعة من المُعلمين المصريين المؤهّلين خريجي جامعة الأزهر الشريف، يملؤهم الشغف والحماس لتدريس المساقات المختلفة في بيئةٍ تعليميةٍ ممتعة وإيجابية.",
+    hAbout4: "اقرأ أكثر",
+    //Courses Section
+    hCourses: "ماذا نُقدِّم",
+    hCoursesDes:
+      "في إيوان، نُقدِّم لك عددًا متنوّعًا من البرامج الدراسية على المستويين: المبتدئ والمتقدم، والتي تناسب توجُّهاتك واحتياجاتك وتساعدك في تحقيق أهدافك المعرفيّة والدراسيّة.",
+    //Islamic Studies
+    islamic: "الدراسات الإسلاميّة",
+    Aqidah: "العقيدة الإسلامية :",
+    Seerah: "السيرة والتاريخ الإسلامي :",
+    Tafseer: "التفسير :",
+    Fiqh: "الفقه :",
+    Hadith: "الحديث :",
+    Manners: "الآداب الإسلامية للأطفال :",
+    //Arabic Studies
+    arabicStudies: "الدراسات العربية",
+    grammar: "قواعد اللغة العربية :",
+    Conversation: "محادثة عربية :",
+    arabicChildren: "العربية للاطفال :",
+    arabicQuran: "العربية للقرآن :",
+    arabicModern: "العربية الفصحى الحديثة :",
+    alSarf: "الصرف :",
+    egyptianDialect: "اللهجة المصرية :",
+    //Quran
+    quran: "القرآن",
+    quranRecitation: "تلاوة القرآن مع التجويد :",
+    quranMemorizing: "حفظ القرآن بالفهم :",
+    Tajweed: "قواعد التجويد :",
+    //Why Section
+    hwhy: "لماذا ايوان",
+    whyOne: "صفوف افتراضية فردية 1:1",
+    whyOneDes:
+      "نقدم الدروس ضمن صفوف افتراضية فردية تضمن التواصل المباشر بين المعلم والطالب",
+    whyPro: "معلمين أكفاء",
+    whyProDes:
+      "يتكون فريقنا من مجموعة من المعلّمين والمعلّمات المؤهلين خريجي الأزهر الشريف ذوي كفاءة عالية",
+    whyFun: "متعة التعلّم",
+    whyFunDes:
+      "التعليم باستخدام أنشطة تحفيزية ومسليّة، وضمن أساليب تعليمية حديثة",
+    whyFlex: "المرونة",
+    whyFlexDes: "مكنك الانضمام إلينا ومن أي مكان ضمن جدول مرن يناسبك",
+    whyFree: "تجربة مجانية",
+    whyFreeDes: "يمكنك الانضمام للدرس الأول في تجربة مجانية دون دفع مسبق",
+    //How Works Section
+    hHowWork: "كيف يعمل",
+    howWorkPlan: "تخطيط",
+    howWorkPlanDes: "اختر دورة واصنع هدفك.",
+    howWorkSched: "تحديد موعد",
+    howWorkSchedDes: "شارك توافرك واجعل جدولك الزمني.",
+    howWorkStudy: "دراسة",
+    howWorkStudyDes: "ادفع وابدأ دروسك.",
+    howWorkSimple: "هو حقا بهذه البساطة!",
+    howWorkStart: "البدء",
+    //FAQS
+    faqs: "الأسئلة الشائعة",
+    faqOne: "ماذا تعني إيوان ؟",
+    faqOneDes: `الكلمة العربية "إيوان" تعني القاعة. إيوان موجود في العديد من المساجد الرائعة حول العالم. يخدم عددًا من الوظائف بما في ذلك الصلاة والتعليم وإيواء المعلمين والطلاب وحفظ الكتب ومكتبة القراءة بالإضافة إلى عدد من الأنشطة الخيرية في محطات التوقف على طريق الحجاج المعدة لخدمة المرضى والمحتاجين . والأهم من ذلك أنها أصبحت المنطقة التي تتشكل فيها دوائر المعرفة التقليدية وتتجمع الطبقات لتمرير العلوم المقدسة ، جيلًا إلى جيل. حتى اليوم العديد من المساجد العظيمة في جميع أنحاء العالم تقيم دروسًا تحت إيوانها.`,
+    faqTwo: "كيف يمكنني البدء ؟",
+    faqTwoDes:
+      "ستكون هناك عملية مقابلة لضمان استفادة الطالب من الخدمات. يرجى الاتصال بنا إذا كنت غير متأكد مما إذا كنت تستوفي الشرط الأساسي ولمزيد من التفاصيل.",
+    faqThree: "ما البرامج أو المعدات التقنية التي أحتاجها ؟",
+    faqThreeDes:
+      "يتم إجراء جميع الفصول الدراسية عبر Zoom، لذلك ستحتاج إلى تنزيل Zoom على الكمبيوتر المحمول أو الكمبيوتر اللوحي أو الهاتف الذكي. نقترح أيضًا الاستثمار في بعض سماعات الرأس عالية الجودة، بالإضافة إلى اتصال جيد بالإنترنت.",
+    faqFour: "هل المعلمون معتمدون ؟",
+    faqFourDes:
+      "نعم، جميع معلمينا معتمدون من ljazah و Huffaz. كما تخرجوا من الجامعات الإسلامية الشهيرة.",
+    faqFive: "هل يمكنني إعادة جدولة الفصول ؟",
+    faqFiveDes:
+      "يسعدنا إعادة جدولة الفصل إذا أعطيتنا إشعارًا لمدة 24 ساعة. يتلقى جميع الطلاب سياسة إعادة الجدولة الخاصة بنا عند التسجيل.",
+    faqSix: "هل يعلم الشيخ النساء ؟",
+    faqSixDes: "نعم. يرجى الاتصال لمزيد من التفاصيل.",
+    faqSeven: "هل تقدم مساعدة مالية ؟",
+    faqSevenDes:
+      "كأكاديمية جديدة، يمكننا فقط تقديم مساعد مالي محدود للغاية. يرجى الاتصال بنا إذا كنت ستواجه صعوبة في الدفع وسنفعل ما في وسعنا للمساعدة. نهدف إلى جعل خدماتنا ميسورة التكلفة، وسنقوم بتدريس العديد من الطلاب المتقدمين مجانًا كل عام، عند المقابلة. يرجى الاتصال بنا لمزيد من المعلومات. إذا كنت ترغب في رعاية طالب،",
+
+    //Footer
+    getTouch: "بإمكانك التواصل معنا",
+    copyRight: "حقوق النشر © 2023. أكاديمية إيوان. جميع الحقوق محفوظة.",
+    //Whatsapp
+    repliesWithin: "يتم الرد عادة في غضون يوم واحد",
+    asSalamu: "السلام عليكم!",
+    WhatsMessage:
+      "انقر أدناه للدردشة معنا على WhatsApp. نحن متاحون للإجابة على أي أسئلة قد تكون لديك حول أكاديمية إيوان.",
+    WhatsChat: "ابدأ الدردشة",
   },
 };
 
